fab8cc8aa0cb90e55d20baf59bd34f2dcf2a3159
Simon Glass <sjg@chromium.org>
2022-05-08 03:14:51 -0600

    wip

--- plugins/vbe/fu-plugin-vbe.c
+++ plugins/vbe/fu-plugin-vbe.c
@@ -10,6 +10,7 @@
 #include "config.h"
 
 #include <fwupdplugin.h>
+#include <libfdt.h>
 
 struct FuPluginData {
 	gchar *vbe_dir;
@@ -32,15 +33,34 @@ fu_plugin_vbe_destroy(FuPlugin *plugin)
 		g_free(priv->vbe_dir);
 }
 
+static void process_bootflow(gchar *buf, gsize len)
+{
+	fdt_check_header(buf);
+}
+
 static gboolean
 fu_plugin_vbe_startup(FuPlugin *plugin, GError **error)
 {
 	FuPluginData *priv = fu_plugin_get_data(plugin);
 	g_autofree gchar *vbe_dir = NULL;
-	g_autofree gchar *cachedir = NULL;
-	cachedir = fu_common_get_path(FU_PATH_KIND_CACHEDIR_PKG);
-	vbe_dir = g_build_filename(cachedir, "vbe", NULL);
+	g_autofree gchar *state_dir = NULL;
+	gchar *buf, *bfname;
+	gsize len;
+
+	/* Get the VBE directory */
+	state_dir = fu_common_get_path(FU_PATH_KIND_LOCALSTATEDIR_PKG);
+	vbe_dir = g_build_filename(state_dir, "vbe", NULL);
 	priv->vbe_dir = g_steal_pointer(&vbe_dir);
+
+	/* Read in the bootflow info */
+	bfname = g_build_filename(priv->vbe_dir, "bootflow.dtb", NULL);
+	if (!g_file_get_contents(bfname, &buf, &len, error)) {
+		g_prefix_error(error, "Could not read '%s'", bfname);
+		return FALSE;
+	}
+	g_log(NULL, G_LOG_LEVEL_INFO, "Processing bootflow '%s'", bfname);
+	process_bootflow(buf, len);
+
 	return TRUE;
 }
 
