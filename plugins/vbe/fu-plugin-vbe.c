029e11c62217115945e6956212be843f29815149
Simon Glass <sjg@chromium.org>
2022-05-30 07:16:47 -0600

    wip

--- plugins/vbe/fu-plugin-vbe.c
+++ plugins/vbe/fu-plugin-vbe.c
@@ -13,19 +13,20 @@
 #include <libfdt.h>
 #include "vbe-simple.h"
 
-/* File to use for system information */
+/* File to use for system information where the system has no device tree */
 #define SYSTEM_DT	"system.dtb"
 
-/* Path to the firmware-update info in the system info */
-#define NODE_PATH	"/chosen/fwupd/firmware"
+/* Path to the update info in the system info */
+#define NODE_PATH	"/chosen/fwupd"
 
-struct vbe_ctx {
+/** Information about an update method with an associated device */
+struct FuVbeMethod {
 	const gchar *vbe_method;
 };
 
 struct FuPluginData {
 	gchar *vbe_dir;
-	const gchar *vbe_method;
+	GList *methods;
 };
 
 static void
@@ -35,6 +36,7 @@ fu_plugin_vbe_init(FuPlugin *plugin)
 	(void)fu_plugin_alloc_data(plugin, sizeof(FuPluginData));
 	priv = fu_plugin_get_data(plugin);
 	priv->vbe_dir = NULL;
+	priv->methods = NULL;
 }
 
 static void
@@ -45,11 +47,37 @@ fu_plugin_vbe_destroy(FuPlugin *plugin)
 		g_free(priv->vbe_dir);
 }
 
+static gboolean vbe_create_device(gchar *fdt, int node,
+				  struct FuVbeMethod **methp)
+{
+	struct FuVbeMethod *meth;
+	const char *compat, *p;
+	int len;
+
+	compat = fdt_getprop(fdt, node, "compatible", &len);
+	if (!compat) {
+		g_error("Missing update mechanism (%s)", fdt_strerror(len));
+		return FALSE;
+	}
+	p = strchr(compat, ',');
+	if (!p) {
+		g_error("Invalid update mechanism (%s)", compat);
+		return FALSE;
+	}
+
+	meth = g_malloc(sizeof(struct FuVbeMethod));
+	meth->vbe_method = p + 1;
+	g_log(G_LOG_DOMAIN, G_LOG_LEVEL_INFO, "Update mechanism: %s",
+	      meth->vbe_method);
+
+	return FALSE;
+}
+
 static gboolean process_system(FuPluginData *priv, gchar *fdt, gsize fdt_len,
 			       GError **error)
 {
-	const char *compat, *p;
-	int ret, node, len;
+	int ret, parent, node;
+	int found;
 
 	ret = fdt_check_header(fdt);
 	if (ret) {
@@ -64,28 +92,36 @@ static gboolean process_system(FuPluginData *priv, gchar *fdt, gsize fdt_len,
 			    fdt_totalsize(fdt), fdt_len);
 		return FALSE;
 	}
-	node = fdt_path_offset(fdt, NODE_PATH);
-	if (node < 0) {
+	parent = fdt_path_offset(fdt, NODE_PATH);
+	if (parent < 0) {
 		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_INVALID_FILE,
 			    "Missing node '%s' (%s)", NODE_PATH,
 			    fdt_strerror(ret));
 		return FALSE;
 	}
-	compat = fdt_getprop(fdt, node, "compatible", &len);
-	if (!compat) {
-		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED,
-			    "Missing update mechanism (%s)", fdt_strerror(len));
-		return FALSE;
+
+	/* Create a device for each subnode */
+	found = 0;
+	for (node = fdt_first_subnode(fdt, parent); node > 0;
+	     node = fdt_next_subnode(fdt, node)) {
+		struct FuVbeMethod *meth;
+
+		if (vbe_create_device(fdt, node, &meth)) {
+			found++;
+		} else {
+			g_warning("Cannot create device for node '%s'",
+				  fdt_get_name(fdt, node, NULL));
+		}
+		priv->methods = g_list_append(priv->methods, meth);
 	}
-	p = strchr(compat, ',');
-	if (!p) {
+
+	if (found) {
+		g_info("VBE update methods: %d", found);
+	} else {
 		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED,
-			    "Invalid update mechanism (%s)", compat);
-		return FALSE;
+			    "No valid VBE update mechanism found");
 	}
-	priv->vbe_method = p + 1;
-	g_log(G_LOG_DOMAIN, G_LOG_LEVEL_INFO, "Update mechanism: %s",
-	      priv->vbe_method);
+
 
 	return TRUE;
 }
