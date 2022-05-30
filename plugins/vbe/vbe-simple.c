b1d732919be0bd8bad8c73ad9e158d975c21c649
Simon Glass <sjg@chromium.org>
2022-05-30 15:24:06 -0600

    wip

--- plugins/vbe/vbe-simple.c
+++ plugins/vbe/vbe-simple.c
@@ -221,7 +221,6 @@ fu_vbe_simple_device_set_progress(FuDevice *self, FuProgress *progress)
 {
 	fu_progress_set_id(progress, G_STRLOC);
 	fu_progress_add_flag(progress, FU_PROGRESS_FLAG_GUESSED);
-	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 100); /* write */
 }
 
 static void
