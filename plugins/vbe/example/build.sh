cc72a65885dff41f6af7b038755511e5e8957c5f
Simon Glass <sjg@chromium.org>
2022-06-05 14:56:52 -0600

    use FIT for updates

--- plugins/vbe/example/build.sh
+++ plugins/vbe/example/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 appstream-util validate-relax com.Vbe.Board.metainfo.xml
-tar -cf firmware.tar my-file
-gcab --create --nopath Vbe-Board-0.0.2.cab firmware.tar com.Vbe.Board.metainfo.xml
+
+dd if=/dev/zero of=update.bin bs=1M count=1
+mkimage -n "v1.2.4" -O U-Boot -A arm64 -C none -T firmware -f auto -d update.bin firmware.fit
+
+gcab --create --nopath Vbe-Board-0.0.2.cab firmware.fit com.Vbe.Board.metainfo.xml
