4d956c30f9ac04bcf3b0ef1a3a94f35d457f889a
Simon Glass <sjg@chromium.org>
2022-06-12 09:07:42 -0600

    wip

--- plugins/vbe/example/build.sh
+++ plugins/vbe/example/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 appstream-util validate-relax com.Vbe.Board.metainfo.xml
 
-dd if=/dev/zero of=update.bin bs=1M count=1
+#dd if=/dev/zero of=update.bin bs=1M count=1
 mkimage -n "v1.2.4" -O U-Boot -A arm64 -C none -T firmware -f auto -d update.bin firmware.fit
 
 gcab --create --zip --nopath Vbe-Board-0.0.2.cab firmware.fit com.Vbe.Board.metainfo.xml
